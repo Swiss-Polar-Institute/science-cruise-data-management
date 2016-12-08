5e946994231873af64962cce397a863a97a6065b
Carles Pina i Estany <carles@pina.cat>
2016-12-08 13:05:12 +0000

    Adds Django extensions.

--- ScienceCruiseDataManagement/ScienceCruiseDataManagement/settings.py
+++ ScienceCruiseDataManagement/ScienceCruiseDataManagement/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'csvimport.app.CSVImportConf',
+     # 'csvimport.app.CSVImportConf',
     'debug_toolbar',
+    'django_extensions',
     'main'  # ScienceCruiseManagement main app
 ]
 
