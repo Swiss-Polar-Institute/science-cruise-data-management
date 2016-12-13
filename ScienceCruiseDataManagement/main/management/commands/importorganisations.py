daf1c4c6c643d7525d3d68e96e6e318303657789
Jen Thomas <jenny_t152@yahoo.co.uk>
2016-12-13 17:24:36 +0000

    Added script to import organisations to database from csv file.

---
+++ ScienceCruiseDataManagement/main/management/commands/importorganisations.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from main.models import Organisation
+import csv
+
+class Command(BaseCommand):
+    help = 'Adds data to the person table'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **options):
+        print(options['filename'])
+        self.import_data_from_csv(options['filename'])
+
+    def import_data_from_csv(self, filename):
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                organisation = Organisation()
+                organisation.name = row['name']
+                organisation.address = row['address']
+                organisation.country = row['country']
+                organisation.save()
+
