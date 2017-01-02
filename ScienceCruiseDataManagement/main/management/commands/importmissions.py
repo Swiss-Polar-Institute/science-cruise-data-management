49e85d193080b89021bb1f4a81a43c8547588f83
Jen Thomas <jenny_t152@yahoo.co.uk>
2017-01-02 13:40:15 +0000

    Imports missions from csv file into missions table.

---
+++ ScienceCruiseDataManagement/main/management/commands/importmissions.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from main.models import Mission
+import csv
+
+class Command(BaseCommand):
+    help = 'Adds data to the mission table'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **options):
+        print(options['filename'])
+        self.import_data_from_csv(options['filename'])
+
+    def import_data_from_csv(self, filename):
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row)
+                mission = Mission()
+                mission.name = row['name']
+                mission.acronym= row['acronym']
+                mission.institution = row['institution']
+                mission.description = row['description']
+
+                mission.save()
