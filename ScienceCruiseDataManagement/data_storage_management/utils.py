a2609565cf201c67c5d904c0dfc0993f5f54eefd
Carles Pina i Estany <carles@pina.cat>
2017-01-05 09:53:09 +0500

    Adds newline in the log message.

--- ScienceCruiseDataManagement/data_storage_management/utils.py
+++ ScienceCruiseDataManagement/data_storage_management/utils.py
@@ -8,7 +8,7 @@ def log(message):
     f = open(os.path.join(os.environ["HOME"], "logs", "importer.log"), "a")
     now = datetime.datetime.now()
     now = now.strftime("%Y-%m-%d %H:%M:%S")
-    f.write("{} {}".format(now, message))
+    f.write("{} {}\n".format(now, message))
     f.close()
 
 
