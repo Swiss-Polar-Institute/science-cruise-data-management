0c699d0b02215fcd881ab7c8fdbb7054af0c9483
Carles Pina i Estany <carles@pina.cat>
2017-01-07 11:39:19 +0500

    Adds a clarification of the time that it takes to process locations.

--- ScienceCruiseDataManagement/main/forms.py
+++ ScienceCruiseDataManagement/main/forms.py
@@ -4,4 +4,5 @@ from django.utils import timezone
 
 class InputShipDateTime(forms.Form):
     ship_date_time = forms.DateTimeField(required=False, label='Date time of the ship (UTC) ')
-    ship_date_times = forms.CharField(help_text="List of date times that could be copied from a spreadsheet and the results copied back", required=False, label='List of date times (UTC)', widget=forms.Textarea(attrs={'cols': "20", 'rows': "10", }))
+    ship_date_times = forms.CharField(help_text="""List of date times that could be copied from a spreadsheet and the results copied back<br>
+                                                Must be in format YYYY-MM-DD HH:MM:SS. Please do in batches of 10 or less for now. It can take up to a minute.""", required=False, label='List of date times (UTC)', widget=forms.Textarea(attrs={'cols': "20", 'rows': "10", }))
