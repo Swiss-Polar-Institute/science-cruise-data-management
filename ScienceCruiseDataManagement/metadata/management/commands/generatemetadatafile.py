3c3fafff2e8406bf676a866a6d01b29fd9199707
Carles Pina i Estany <carles@pina.cat>
2017-02-13 16:59:28 -0800

    Generates part of the metadata XML.

--- ScienceCruiseDataManagement/metadata/management/commands/generatemetadatafile.py
+++ ScienceCruiseDataManagement/metadata/management/commands/generatemetadatafile.py
@@ -18,10 +18,34 @@ class ExportMetadataFile:
         self.metadata_entry = MetadataEntry.objects.get(id=self.metadata_id)
         self.xml_root = None
 
-    def add_char_element(self, tag, field):
-        element = etree.SubElement(self.xml_root, tag)
-        element.text = getattr(self.metadata_entry, field)
-        self.xml_root.append(element)
+    def add_char_element(self, parent, tag, text):
+        if text != "" and text is not None:
+            element = etree.SubElement(parent, tag)
+            element.text = text
+
+    def add_date_element(self, parent, tag, date):
+        if date is not None:
+            element = etree.SubElement(parent, tag)
+            element.text = date.strftime("%F")
+
+    def add_data_set_citation(self, parent, tag, data_set_citations):
+        for data_set_citation in data_set_citations.all():
+            data_set_citation_xml = etree.SubElement(parent, tag)
+
+            self.add_char_element(data_set_citation_xml, "Dataset_Creator",
+                                  "{} {}".format(data_set_citation.dataset_creator.name_first, data_set_citation.dataset_creator.name_last))
+            self.add_char_element(data_set_citation_xml, "Dataset_Title", data_set_citation.dataset_title)
+            self.add_date_element(data_set_citation_xml, "Dataset_Release_Date", data_set_citation.dataset_release_date)
+            self.add_char_element(data_set_citation_xml, "Dataset_Publisher", data_set_citation.dataset_publisher)
+            self.add_char_element(data_set_citation_xml, "Version", data_set_citation.version)
+            self.add_char_element(data_set_citation_xml, "Other_Citation_Details", data_set_citation.other_citation_details)
+
+    def add_sensor_names(self, parent, tag, sensor_names):
+        for sensor_name in sensor_names.all():
+            print(sensor_name.uuid)
+            sensor_name_xml = etree.SubElement(parent, tag, {'uuid': sensor_name.uuid})
+            self.add_char_element(sensor_name_xml, 'Short_Name', sensor_name.short_name)
+            self.add_char_element(sensor_name_xml, 'Long_Name', sensor_name.long_name)
 
     def do(self):
         fp = open("/tmp/test_dif.xml", "wb")
@@ -30,55 +54,23 @@ class ExportMetadataFile:
                  "xsi": "http://www.w3.org/2001/XMLSchema-instance",
                  "schemaLocation": "http://gcmd.nasa.gov/Aboutus/xml/dif/dif_v9.7.1.xsd"
                  }
-        self.xml_root = etree.Element("DIF", nsmap=nsmap)
+        xml_root = etree.Element("DIF", nsmap=nsmap)
 
-        self.add_char_element('Entry_ID', 'entry_id')
-        self.add_char_element('Entry_Title', 'entry_title')
-        self.add_char_element('Quality', 'quality')
-        self.add_char_element('Access_Constraints', 'access_constraints')
-        self.add_char_element('Use_Constraints', 'use_constraints')
-        self.add_char_element('Data_Set_Language', 'data_set_language')
-        self.add_char_element('Originating_Center', 'originating_center')
-        self.add_char_element('Parent_DIF', 'parent_dif')
-        self.add_char_element('Metadata_Name', 'metadata_name')
-        self.add_char_element('Metadata_Version', 'metadata_version')
-        self.add_char_element('Dif_Revision_History', 'dif_revision_history')
+        self.add_char_element(xml_root, 'Entry_ID', self.metadata_entry.entry_id)
+        self.add_char_element(xml_root, 'Entry_Title', self.metadata_entry.entry_title)
+        self.add_char_element(xml_root, 'Quality', self.metadata_entry.quality)
+        self.add_char_element(xml_root, 'Access_Constraints', self.metadata_entry.access_constraints)
+        self.add_char_element(xml_root, 'Use_Constraints', self.metadata_entry.use_constraints)
+        self.add_char_element(xml_root, 'Data_Set_Language', self.metadata_entry.data_set_language)
+        self.add_char_element(xml_root, 'Originating_Center', self.metadata_entry.originating_center)
+        self.add_char_element(xml_root, 'Parent_DIF', self.metadata_entry.parent_dif)
+        self.add_char_element(xml_root, 'Metadata_Name', self.metadata_entry.metadata_name)
+        self.add_char_element(xml_root, 'Metadata_Version', self.metadata_entry.metadata_version)
+        self.add_char_element(xml_root, 'Dif_Revision_History', self.metadata_entry.dif_revision_history)
+        self.add_data_set_citation(xml_root, 'Data_Set_Citation', self.metadata_entry.data_set_citation)
+        self.add_sensor_names(xml_root, 'Sensor_Name', self.metadata_entry.sensor_name)
 
-        b = etree.tostring(self.xml_root, pretty_print=True)
+        b = etree.tostring(xml_root, pretty_print=True)
         print(b.decode('utf-8'))
         fp.write(b)
         fp.close()
-
-
-        # entry_id = etree.SubElement(root, "Entry_ID")
-        # entry_id.text = metadata_entry.entry_id
-        # root.append(entry_id)
-        #
-        # # entry_title = etree.SubElement(root, "Entry_Title")
-        # # entry_title.text = "Rock outcrop map of the Antarctic continent derived from Landsat 8 imagery"
-        # # root.append(entry_title)
-        # #
-        # # entry_summary = etree.SubElement(root, "Summary")
-        # # entry_summary.text = """Test
-        # # Hello
-        # # <test>
-        # #
-        # # Test
-        # #         """
-        # # root.append(entry_summary)
-        # #
-        # # temporal_coverage = etree.SubElement(root, "Temporal_Coverage")
-        # # start_date = etree.SubElement(temporal_coverage, "Start_Date")
-        # # start_date.text = "2016-07-27"
-        # # temporal_coverage.append(start_date)
-        # #
-        # # end_date = etree.SubElement(temporal_coverage, "End_Date")
-        # # end_date.text = "2016-07-27"
-        # # temporal_coverage.append(end_date)
-        # #
-        # # root.append(temporal_coverage)
-        #
-        # b = etree.tostring(root, pretty_print=True)
-        # fp.write(b)
-        # fp.close()
-
