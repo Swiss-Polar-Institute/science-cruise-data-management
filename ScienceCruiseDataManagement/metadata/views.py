98f8ed9e9e22d446ae756da4056074d96bc9ce23
Carles Pina i Estany <carles@pina.cat>
2017-02-28 18:45:47 -0200

    Adds a link to export metadata entry as Microsoft Word format. Needs pandoc

--- ScienceCruiseDataManagement/metadata/views.py
+++ ScienceCruiseDataManagement/metadata/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 import datetime
 from main.models import Project
 from metadata.models import *
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, View
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
+from django.template import loader
 import types
+from django.views.static import serve
+import subprocess
+
 
 class ProjectListView(ListView):
     model = Project
@@ -26,79 +30,111 @@ class MetadataEntryListView(ListView):
         return context
 
 
+def metadata_entry_context(id):
+    try:
+        metadata_entry = MetadataEntry.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return "No metadata found"
+
+    rows = []
+
+    rows.append(('ID', render_object(metadata_entry.entry_id)))
+
+    rows.append(('Title', render_object(metadata_entry.entry_title)))
+
+    rows.append(('Data set citation', render_object(metadata_entry.data_set_citation, "<br>")))
+
+    rows.append(('Personnel', render_object(metadata_entry.personnel)))
+
+    rows.append(('Parameters', render_object(metadata_entry.parameters)))
+
+    rows.append(('Sensor names', render_object(metadata_entry.sensor_name)))
+
+    rows.append(('Source names', render_object(metadata_entry.source_name)))
+
+    rows.append(('Temporal coverage', render_object(metadata_entry.temporal_coverage)))
+
+    rows.append(('Data set progress', render_object(metadata_entry.data_set_progress)))
+
+    rows.append(('Spatial coverage', render_object(metadata_entry.spatial_coverage)))
+
+    rows.append(('Location', render_object(metadata_entry.location)))
+
+    rows.append(('Data resolution', render_object(metadata_entry.data_resolution)))
+
+    rows.append(('Projects', render_object(metadata_entry.project)))
+
+    rows.append(('Quality', render_object(metadata_entry.quality, "<br>")))
+
+    rows.append(('Access constraints', render_object(metadata_entry.access_constraints)))
+
+    rows.append(('Use constraints', render_object(metadata_entry.use_constraints)))
+
+    rows.append(('Data set language', render_object(metadata_entry.data_set_language)))
+
+    rows.append(('Originating center', render_object(metadata_entry.originating_center)))
+
+    rows.append(('Data center', render_object(metadata_entry.data_center)))
+
+    # rows.append(('Distribution', render_object(metadata_entry.distribution.all())))
+    rows.append(('Distribution', "TODO view"))
+
+    rows.append(('Summary', render_object(metadata_entry.summary)))
+
+    rows.append(('Parent DIF', render_object(metadata_entry.parent_difs)))
+
+    rows.append(('IDN node', render_object(metadata_entry.idn_node)))
+
+    rows.append(('Metadata name', render_object(metadata_entry.metadata_name)))
+
+    rows.append(('Metadata version', render_object(metadata_entry.metadata_version)))
+
+    rows.append(('DIF creation date', render_object(metadata_entry.dif_creation_date)))
+
+    rows.append(('Last DIF revision date', render_object(metadata_entry.last_dif_revision_date)))
+
+    rows.append(('DIF revision history', render_object(metadata_entry.dif_revision_history)))
+
+    rows.append(('Future DIF review date', render_object(metadata_entry.future_dif_review_date)))
+
+    rows.append(('Private', render_object(metadata_entry.private)))
+
+    context = {}
+    context['rows'] = rows
+    context['metadata_entry_id'] = id
+    return context
+
+
+class MetadataEntryAsWord(TemplateView):
+    def get(self, request, *args, **kwargs):
+        id = args[0]
+        context = metadata_entry_context(id)
+        context['exporting'] = True
+        content = loader.render_to_string("metadata_entry.html",
+                                          context, request)
+
+        f = open("/tmp/content.html", "w")
+        f.write(content)
+        f.close()
+
+        s = subprocess.Popen(["pandoc", "-o", "/tmp/content.docx", "/tmp/content.html"])
+        s.wait()
+
+        metadata_entry = MetadataEntry.objects.get(id=id)
+
+        filename = "metadata-{}.docx".format(metadata_entry.entry_id)
+
+        s = serve(request, "content.docx", "/tmp")
+        s._headers['content-disposition'] = (('Content-Disposition', 'attachment; filename="{}"'.format(filename)))
+        return s
+
 class MetadataEntryView(TemplateView):
     template_name = "metadata_entry.html"
 
     def get(self, request, *args, **kwargs):
         id = int(args[0])
-        try:
-            metadata_entry = MetadataEntry.objects.get(id=id)
-        except ObjectDoesNotExist:
-            pass
 
-        rows = []
-
-        rows.append(('ID', render_object(metadata_entry.entry_id)))
-        rows.append(('Title', render_object(metadata_entry.entry_title)))
-
-        rows.append(('Data set citation', render_object(metadata_entry.data_set_citation, "<br>")))
-
-        rows.append(('Personnel', render_object(metadata_entry.personnel)))
-
-        rows.append(('Parameters', render_object(metadata_entry.parameters)))
-
-        rows.append(('Sensor names', render_object(metadata_entry.sensor_name)))
-
-        rows.append(('Source names', render_object(metadata_entry.source_name)))
-
-        rows.append(('Temporal coverage', render_object(metadata_entry.temporal_coverage)))
-
-        rows.append(('Data set progress', render_object(metadata_entry.data_set_progress)))
-
-        rows.append(('Spatial coverage', render_object(metadata_entry.spatial_coverage)))
-
-        rows.append(('Location', render_object(metadata_entry.location)))
-
-        rows.append(('Data resolution', render_object(metadata_entry.data_resolution)))
-
-        rows.append(('Projects', render_object(metadata_entry.project)))
-
-        rows.append(('Quality', render_object(metadata_entry.quality, "<br>")))
-
-        rows.append(('Access constraints', render_object(metadata_entry.access_constraints)))
-
-        rows.append(('Use constraints', render_object(metadata_entry.use_constraints)))
-
-        rows.append(('Data set language', render_object(metadata_entry.data_set_language)))
-
-        rows.append(('Originating center', render_object(metadata_entry.originating_center)))
-
-        rows.append(('Data center', render_object(metadata_entry.data_center)))
-
-        # rows.append(('Distribution', render_object(metadata_entry.distribution.all())))
-        rows.append(('Distribution', "TODO view"))
-
-        rows.append(('Summary', render_object(metadata_entry.summary)))
-
-        rows.append(('Parent DIF', render_object(metadata_entry.parent_difs)))
-
-        rows.append(('IDN node', render_object(metadata_entry.idn_node)))
-
-        rows.append(('Metadata name', render_object(metadata_entry.metadata_name)))
-
-        rows.append(('Metadata version', render_object(metadata_entry.metadata_version)))
-
-        rows.append(('DIF creation date', render_object(metadata_entry.dif_creation_date)))
-
-        rows.append(('Last DIF revision date', render_object(metadata_entry.last_dif_revision_date)))
-
-        rows.append(('DIF revision history', render_object(metadata_entry.dif_revision_history)))
-
-        rows.append(('Future DIF review date', render_object(metadata_entry.future_dif_review_date)))
-
-        rows.append(('Private', render_object(metadata_entry.private)))
-
-        return render(request, "metadata_entry.html", {'rows': rows, 'metadata_entry_id': id})
+        return render(request, "metadata_entry.html", metadata_entry_context(id))
 
 
 def get_attribute_from_field(object, field):
