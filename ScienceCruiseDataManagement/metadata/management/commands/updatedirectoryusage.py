7381aabd5addde327ef91fc8f31be7870cc22018
Carles Pina i Estany <carles@pina.cat>
2017-03-01 12:28:00 -0200

    Adds a command line argument to the updatedirectoryusage to skip things and only validates the directories.

--- ScienceCruiseDataManagement/metadata/management/commands/updatedirectoryusage.py
+++ ScienceCruiseDataManagement/metadata/management/commands/updatedirectoryusage.py
@@ -9,6 +9,11 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('metadata_id', type=str, help="Can be all for all the metadata")
+        parser.add_argument('--dry-run',
+                            action='store_true',
+                            dest='dry_run',
+                            default=False,
+                            help="Skips updating the database and counting everything. Useful to find directories that don't exist.")
 
     def handle(self, *args, **options):
         metadata_id = options['metadata_id']
@@ -19,23 +24,26 @@ class Command(BaseCommand):
             metadata_entries = MetadataEntry.objects.filter(entry_id=metadata_id)
 
         for metadata_entry in metadata_entries:
-            distribution_size = DistributionSizeUpdater(metadata_entry)
+            distribution_size = DistributionSizeUpdater(metadata_entry, options['dry_run'])
             distribution_size.do()
 
+
 class DistributionSizeUpdater:
-    def __init__(self, metadata_entry):
-        self.metadata_entry = metadata_entry
+    def __init__(self, metadata_entry, dry_run):
+        self._metadata_entry = metadata_entry
+        self._dry_run = dry_run
 
     def do(self):
-        print("Will start processing:", self.metadata_entry.entry_id)
-        files = self._files_for_metadata_entry(self.metadata_entry)
+        print("Will start processing:", self._metadata_entry.entry_id)
+        files = self._files_for_metadata_entry(self._metadata_entry)
         size = self.calculate_size(files)
 
         print("Total size: {:.2f} GB".format(size/1024/1024/1024))
 
-        for distribution in Distribution.objects.filter(metadata_entry=self.metadata_entry):
+        for distribution in Distribution.objects.filter(metadata_entry=self._metadata_entry):
             distribution.distribution_size = size
-            distribution.save()
+            if not self._dry_run:
+                distribution.save()
 
     def calculate_size(self, files):
         size = 0
@@ -70,9 +78,10 @@ class DistributionSizeUpdater:
                 print("** Path: {} doesn't exist. Skipping".format(absolute_path))
                 continue
 
-            for (dirpath, dirnames, filenames) in os.walk(self.absolute_directory(directory)):
-                for filename in filenames:
-                    absolute_path_file = os.path.join(dirpath, filename)
-                    files.add(absolute_path_file)
+            if not self._dry_run:
+                for (dirpath, dirnames, filenames) in os.walk(self.absolute_directory(directory)):
+                    for filename in filenames:
+                        absolute_path_file = os.path.join(dirpath, filename)
+                        files.add(absolute_path_file)
 
         return files
