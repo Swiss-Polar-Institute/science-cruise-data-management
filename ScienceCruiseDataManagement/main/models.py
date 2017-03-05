9522af3fcb7930ed2e3dc02c88486613cd78f8a9
Carles Pina i Estany <carles@pina.cat>
2017-03-05 12:49:54 -0100

    Enables save_as for the IslandLandings model.

--- ScienceCruiseDataManagement/main/models.py
+++ ScienceCruiseDataManagement/main/models.py
@@ -70,6 +70,9 @@ class IslandLandings(models.Model):
     class Meta:
         verbose_name_plural = "Island Landings"
 
+    save_as = True
+
+
 class DeviceType(models.Model):
     url = models.CharField(max_length=255, null=True, blank=True, help_text = "If adding a new device leave this field blank.")
     code = models.CharField(max_length=255, unique=True, null=True, blank=True, help_text = "If adding a new device leave this field blank.")
