022a7877b27dbac6e147bab04a3802ac90dc1bbf
Jen Thomas <jenny_t152@yahoo.co.uk>
2017-08-21 10:22:17 +0100

    Added postcruisedatacontact table.

--- ScienceCruiseDataManagement/data_administration/admin.py
+++ ScienceCruiseDataManagement/data_administration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+import data_administration.models
+
+
+class PostCruiseDataContactAdmin(admin.ModelAdmin):
+    list_display = ('person', 'project', 'created_on')
 
 # Register your models here.
+admin.site.register(data_administration.models.PostCruiseDataContact, PostCruiseDataContactAdmin)
+
--- ScienceCruiseDataManagement/data_administration/models.py
+++ ScienceCruiseDataManagement/data_administration/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from main.models import Person, Project
+import django.utils.timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# This file is part of https://github.com/Swiss-Polar-Institute/science-cruise-data-management
+
+class PostCruiseDataContact(models.Model):
+    person = models.ForeignKey(Person)
+    project = models.ForeignKey(Project)
+    created_on = models.DateTimeField(default=django.utils.timezone.now)
+
+    class Meta:
+        unique_together = (('person', 'project'),)
+
+    def __str__(self):
+        return "{}".format(self.person, self.project)
+
