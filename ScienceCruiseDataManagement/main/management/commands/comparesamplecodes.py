daf4a70274bad8911e769a48990c77a24af37d1b
Jen Thomas <jenny_t152@yahoo.co.uk>
2017-09-29 21:01:22 +0100

    Created command to compare sample codes from database and lists.

--- ScienceCruiseDataManagement/main/management/commands/comparesamplecodes.py
+++ ScienceCruiseDataManagement/main/management/commands/comparesamplecodes.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 from main.models import Sample, Project, SamplingMethod
-# import the function from createreports.reportproject.save_into_file - actually probably don't need this becasue I want a list rather than the file
 
 import csv
 
-# add the class command here (look at exportgps tracks command for help)
 class Command(BaseCommand):
     help = 'Compare lists of samples'
 
@@ -12,7 +10,7 @@ class Command(BaseCommand):
         parser.add_argument('data_file_directory', type=str, help="Type in the name of the directory in which the data file can be found")
         parser.add_argument('data_filename', type=str, help="Type in the name of the data file")
         parser.add_argument('project_number', type=int, help="Project number to analyse samples from")
-        parser.add_argument('sampling_method_name', type=str, help="Method by which the samples were collected")
+        parser.add_argument('--sampling_method_name', type=str, help="Method by which the samples were collected")
 
     def handle(self, *args, **options):
         data_file_directory = options['data_file_directory']
@@ -20,19 +18,24 @@ class Command(BaseCommand):
         project_number = options['project_number']
         sampling_method_name = options['sampling_method_name']
 
-        compareSampleLists = CompareSampleLists.compare_sample_lists(sampling_method_name, project_number, data_file_directory, data_filename)
+        compareSampleLists = CompareSampleLists.compare_sample_lists(project_number, sampling_method_name, data_file_directory, data_filename)
+
 
 class CompareSampleLists():
     '''Compares two lists of sample codes, one from the database and one from a set of data, and outputs lists of sample codes that meet certain criteria'''
 
     @staticmethod
-    def create_sample_list_from_database(sampling_method_name, project_number):
+    def create_sample_list_from_database(project_number, sampling_method_name):
         '''This function is very similar and modified from to Command.samples from createreports.py. It gets a list of samples from the database following the criteria in the function set-up'''
 
         sample_numbers = []
-
-        for sample in Sample.objects.filter(event__sampling_method__name=sampling_method_name).filter(project__number=project_number).order_by('expedition_sample_code'):
-            sample_numbers.append(sample.expedition_sample_code)
+        print(sampling_method_name)
+        if not sampling_method_name:
+            for sample in Sample.objects.filter(project__number=project_number):
+                sample_numbers.append(sample.expedition_sample_code)
+        else:
+            for sample in Sample.objects.filter(event__sampling_method__name=sampling_method_name).filter(project__number=project_number).order_by('expedition_sample_code'):
+                sample_numbers.append(sample.expedition_sample_code)
 
         return sample_numbers
 
@@ -115,10 +118,10 @@ class CompareSampleLists():
 
 
     @staticmethod
-    def compare_sample_lists(sampling_method_name, project_number, data_file_directory, data_filename):
+    def compare_sample_lists(project_number, sampling_method_name, data_file_directory, data_filename):
         '''This function creates a list of samples from the database and compares it to a list of samples from an input file. It outputs lists samples that match and do not match between the two lists'''
 
-        database_sample_list = CompareSampleLists.create_sample_list_from_database(sampling_method_name, project_number)
+        database_sample_list = CompareSampleLists.create_sample_list_from_database(project_number, sampling_method_name)
 
         file_sample_list = CompareSampleLists.get_list_of_data_samples(data_file_directory, data_filename)
 
