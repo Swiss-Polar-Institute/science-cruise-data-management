81b8354de49e3887520b9503e45d733754758210
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2017-10-16 15:58:42 +0200

    update wsgi.py

--- ScienceCruiseDataManagement/ScienceCruiseDataManagement/wsgi.py
+++ ScienceCruiseDataManagement/ScienceCruiseDataManagement/wsgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+activate_this = '/var/www/vhosts/scdm.epfl.ch/private/virtenv/scdm-env/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+path = '/var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement'
+if path not in sys.path:
+sys.path.append(path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScienceCruiseDataManagement.default_settings")
