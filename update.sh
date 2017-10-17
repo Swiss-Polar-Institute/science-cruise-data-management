899f4cc399de060954292d22dd5057d8c8b989a6
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2017-10-17 13:36:24 +0200

    add update.sh script

---
+++ update.sh
@@ -0,0 +1,16 @@
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' ScienceCruiseDataManagement/ /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement
+
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' requirements.txt /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/requirements.txt
+
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' scdm.conf /var/www/vhosts/scdm.epfl.ch/conf/scdm.conf
+
+ln -sf /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement/settings_prod.py /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement/default.py
+
+source /opt/rh/rh-python35/enable
+source /var/www/vhosts/scdm.epfl.ch/private/virtenv/scdm-env/bin/activate
+
+pip install -r /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/requirements.txt
+
+python /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/manage.py collectstatic
+
+sudo systemctl restart httpd24-http
