f9fd91e5b88a60ab7d034ea47641a09c4dac0d18
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2017-10-17 13:38:41 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
-rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' ScienceCruiseDataManagement/ /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='- *.pyc' ScienceCruiseDataManagement/ /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement
 
-rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' requirements.txt /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/requirements.txt
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='- *.pyc' requirements.txt /var/www/vhosts/scdm.epfl.ch/private/requirements.txt
 
-rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='-*.pyc' scdm.conf /var/www/vhosts/scdm.epfl.ch/conf/scdm.conf
+rsync -r -l -z -t --delete --delete-excluded --filter='+ */' --filter='- *.pyc' scdm.conf /var/www/vhosts/scdm.epfl.ch/conf/scdm.conf
 
 ln -sf /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement/settings_prod.py /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/ScienceCruiseDataManagement/default.py
 
 source /opt/rh/rh-python35/enable
 source /var/www/vhosts/scdm.epfl.ch/private/virtenv/scdm-env/bin/activate
 
-pip install -r /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/requirements.txt
+pip install -r /var/www/vhosts/scdm.epfl.ch/private/requirements.txt
 
 python /var/www/vhosts/scdm.epfl.ch/private/ScienceCruiseDataManagement/manage.py collectstatic
 
