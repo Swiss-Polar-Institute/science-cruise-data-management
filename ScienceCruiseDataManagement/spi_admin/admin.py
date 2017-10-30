141041bf9c42cb5baab1d8ea7de4edfe9b61e57d
Jen Thomas <jenny_t152@yahoo.co.uk>
2017-10-30 16:54:32 +0000

    Created mailing list table in spi_admin.

--- ScienceCruiseDataManagement/spi_admin/admin.py
+++ ScienceCruiseDataManagement/spi_admin/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+import spi_admin.models
+
 
 # Register your models here.
+
+class MailingListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    ordering = ['name']
+
+
+admin.site.register(spi_admin.models.MailingList, MailingListAdmin)
--- ScienceCruiseDataManagement/spi_admin/models.py
+++ ScienceCruiseDataManagement/spi_admin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from main.models import Person, PersonRole, Mission, Project
 
 # Create your models here.
+
+class MailingList(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    
