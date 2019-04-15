275db2dd829a97357cf5c77ac3695903f9a9a4d6
Carles Pina i Estany <carles@pina.cat>
2019-04-15 13:09:10 +0200

    File to reset the database, migrations, make migrations, migrate and create super user

---
+++ ScienceCruiseDataManagement/reset-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "drop database glace; create database glace;" | python3 manage.py dbshell
+
+rm -rf main/migrations/
+rm -rf ship_data/migrations/
+rm -rf data_storage_management/migrations/
+rm -rf ctd/migrations/
+rm -rf underway_sampling/migrations/
+rm -rf metadata/migrations/
+
+python3 manage.py makemigrations metadata ship_data data_storage_management ctd underway_sampling main
+python3 manage.py migrate 
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
