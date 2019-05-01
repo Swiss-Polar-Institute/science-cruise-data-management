c3fcb2863c298dc941887b4109deef8363a7d2b7
Carles Pina i Estany <carles@pina.cat>
2019-05-01 10:23:38 +0100

    Some error control.

--- ScienceCruiseDataManagement/main/views.py
+++ ScienceCruiseDataManagement/main/views.py
@@ -440,24 +440,32 @@ class SunsetSunrise(TemplateView):
 
     def post(self, request, *args, **kwargs):
         template_information = {}
-        template_information['latitude'] = request.POST['latitude']
-        template_information['longitude'] = request.POST['longitude']
+
+        error_message = ""
+        try:
+            latitude = float(request.POST['latitude'])
+            longitude = float(request.POST['longitude'])
+        except ValueError:
+            error_message += "Latitude and longitude need to be in decimal degrees. E.g. 78.42 or -10.24"
+
+        try:
+            date = datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d").date()
+        except ValueError:
+            date = datetime.datetime.today()
+            error_message += "Invalid date time. It needs to be YYYY-MM-DD"
+
+        if error_message != "":
+            form = InputPositionDate()
+            return render(request, "sunset_sunrise.html", {'form': form,
+                                                           'error_message': error_message})
+
+        template_information['latitude'] = latitude
+        template_information['longitude'] = longitude
         template_information['date'] = request.POST['date']
 
-        error = ""
-
-        if request.POST['date'] != "":
-            try:
-                date = datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d").date()
-            except ValueError:
-                date = datetime.datetime.today()
-                error = "Invalid date time"
-        else:
-            date = datetime.datetime.today()
-
         place = astral.Location()
-        place.latitude = float(request.POST['latitude'])
-        place.longitude = float(request.POST['longitude'])
+        place.latitude = latitude
+        place.longitude = longitude
 
         template_information['sunrise_utc'] = place.sunrise(date=date)
         template_information['dawn_utc'] = place.dawn(date=date)
@@ -471,11 +479,10 @@ class SunsetSunrise(TemplateView):
         template_information['sunset_ship_time'] = utils.format_date_time(utils.date_utc_to_ship_time(place.sunset(date=date)))
         template_information['date_parsed'] = date.strftime("%Y-%m-%d")
 
-        template_information['error'] = error
+        template_information['error_message'] = error_message
 
         template_information['form'] = InputPositionDate(initial={'latitude': place.latitude,
                                                                   'longitude': place.longitude})
-
 
         return render(request, "sunset_sunrise_exec.html", template_information)
 
