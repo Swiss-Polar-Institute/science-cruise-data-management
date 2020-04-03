725edba4df5f3d54fcf1b1bc7668fac7d4e8fc4c
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-03 12:56:07 +0200

    Added input arguments from user to get sample codes command.

---
+++ ScienceCruiseDataManagement/main/management/commands/getacesamplecode.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from main.models import Sample
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Get ACE sample code where a project sample code is provided'
+
+    def add_arguments(self, parser):
+        parser.add_argument('input_filename', help="Filename containing the project sample codes", type=str)
+        parser.add_argument('output_filename', help="Filename to output the data into", type=str)
+
+    def handle(self, *args, **options):
+        process_input_file(options['input_filename'], options['output_filename'])
+
+def process_input_file(input_filename, output_filename):
+    pass
+
