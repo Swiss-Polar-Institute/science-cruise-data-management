d359ac450f3a78cfce70143f7f5e2819a31747db
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-03 23:16:32 +0200

    First attempt to get expedition sample code. Not working yet.

--- ScienceCruiseDataManagement/main/management/commands/getacesamplecode.py
+++ ScienceCruiseDataManagement/main/management/commands/getacesamplecode.py
@@ -4,7 +4,7 @@ import csv
 
 
 class Command(BaseCommand):
-    help = 'Get ACE sample code where a project sample code is provided'
+    help = 'Get expedition sample code where a project sample code is provided'
 
     def add_arguments(self, parser):
         parser.add_argument('input_filename', help="Filename containing the project sample codes", type=str)
@@ -13,6 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         process_input_file(options['input_filename'], options['output_filename'])
 
+
 def process_input_file(input_filename, output_filename):
     with open(input_filename, 'r') as data_file:
         contents = csv.reader(data_file)
@@ -20,10 +21,15 @@ def process_input_file(input_filename, output_filename):
         with open(output_filename, 'w') as output_file:
             writer = csv.writer(output_file)
 
-            header = ['project_sample_code', 'ace_sample_code']
+            header = ['project_sample_code', 'expedition_sample_code']
             writer.writerow(header)
 
             for line in contents:
                 # read in the project sample code
                 project_sample_code = line[0]
                 print(project_sample_code)
+
+                # get the associated expedition sample code. Note that on some occasions the project sample code is not unique
+                # and there is often no way to get more information from the project sample code. Or at least no unique
+                # way.
+                expedition_sample_code = Sample.get(project_sample_number=project_sample_code)
