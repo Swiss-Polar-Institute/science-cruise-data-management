5c6c87558ecce631ddac8efcfbeaf433614ef135
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-04-03 12:58:38 +0200

    Read input from file of project sample codes and write an outfile with a header.

--- ScienceCruiseDataManagement/main/management/commands/getacesamplecode.py
+++ ScienceCruiseDataManagement/main/management/commands/getacesamplecode.py
@@ -14,5 +14,16 @@ class Command(BaseCommand):
         process_input_file(options['input_filename'], options['output_filename'])
 
 def process_input_file(input_filename, output_filename):
-    pass
+    with open(input_filename, 'r') as data_file:
+        contents = csv.reader(data_file)
 
+        with open(output_filename, 'w') as output_file:
+            writer = csv.writer(output_file)
+
+            header = ['project_sample_code', 'ace_sample_code']
+            writer.writerow(header)
+
+            for line in contents:
+                # read in the project sample code
+                project_sample_code = line[0]
+                print(project_sample_code)
